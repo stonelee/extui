37f5ed1b8b84adbdd64ea12592689119b360bbbe
stonelee <istonelee@gmail.com>
2012-12-06 10:04:03 +0800

    calendar增加target参数用来适应input+i的情形

--- calendar/examples/index.md
+++ calendar/examples/index.md
@@ -5,16 +5,18 @@
 ````iframe:250
 <link href="http://10.10.22.84/work/extui/css/main.css" rel="stylesheet">
 
-<input id="date-nothing" type="text" />
+<input id="date" type="text"><i id="date-trigger" class="form-trigger form-date-trigger"></i>
 
 <script>
 seajs.config({
-    locale: 'zh-cn',
-    preload: ['seajs/plugin-i18n']
+  locale: 'zh-cn',
+  preload: ['seajs/plugin-i18n']
 });
 
-seajs.use('calendar', function(Calendar) {
-    new Calendar({trigger: '#date-nothing'});
+seajs.use(['$','calendar'], function($,Calendar) {
+  var calendar = new Calendar({
+    target: '#date'
+  });
 });
 </script>
 ````
--- calendar/src/calendar.js
+++ calendar/src/calendar.js
@@ -6,7 +6,48 @@ define(function(require, exports, module) {
 
   var Calendar = AraleCalendar.extend({
     attrs: {
-      template: template
+      //output为target，trigger为target后面的元素
+      target: null,
+
+      template: template,
+
+      trigger: {
+        value: '',
+        getter: function(val) {
+          val = val ? val : $(this.get('target')).next();
+          return $(val);
+        }
+      },
+      output: {
+        value: '',
+        getter: function(val) {
+          val = val ? val : this.get('target');
+          return $(val);
+        }
+      },
+
+      align: {
+        getter: function() {
+          var target = this.get('target');
+          if (target) {
+            return {
+              selfXY: [0, 0],
+              baseElement: target,
+              baseXY: [0, $(target).height() + 5]
+            };
+          }
+          return {
+            selfXY: [0, 0],
+            baseXY: [0, 0]
+          };
+        }
+      }
+    },
+
+    setup: function() {
+      Calendar.superclass.setup.call(this);
+      //不知道点击其他地方时为什么无法自动关闭
+      this._blurHide([this.get('trigger')]);
     }
 
   });
