436727fbced5a1163ec22dff0429db6b16d03f36
stonelee <istonelee@gmail.com>
2012-12-10 11:36:31 +0800

    change dialog

--- dialog/dist/dialog-debug.js
+++ dialog/dist/dialog-debug.js
@@ -9,7 +9,7 @@ define("kj/dialog/0.0.1/dialog-debug", ["$-debug", "arale/overlay/0.9.12/mask-de
 
   var Dialog = ConfirmBox.extend({
     attrs: {
-      template: '<div class="window" style="left:50px;top:150px;"> {{#if hasCloseX}}<i class="icon icon-tool icon-tool-close" data-role="close"></i>{{/if}} {{#if hasTitle}} <div class="window-header unselectable" data-role="head"> <span data-role="title">{{title}}</span> </div> {{/if}} <div class="window-body"> {{#if icon}} <i class="icon icon-{{icon}}"></i> {{/if}} <span data-role="content">{{content}}</span> {{#if hasFoot}} <div class="window-toolbar" data-role="foot"> {{#if hasOk}}<button class="btn" data-role="confirm">确定</button>{{/if}} {{#if hasCancel}}<button class="btn" data-role="cancel">取消</button>{{/if}} </div> {{/if}} </div> </div>',
+      template: '<div class="dialog" style="left:50px;top:150px;"> {{#if hasCloseX}}<i class="icon icon-tool icon-tool-close" data-role="close"></i>{{/if}} {{#if hasTitle}} <div class="dialog-hd unselectable" data-role="head"> <span data-role="title">{{title}}</span> </div> {{/if}} <div class="dialog-bd"> {{#if icon}} <i class="icon icon-{{icon}}"></i> {{/if}} <span data-role="content">{{content}}</span> {{#if hasFoot}} <div class="dialog-toolbar" data-role="foot"> {{#if hasOk}}<button class="btn" data-role="confirm">确定</button>{{/if}} {{#if hasCancel}}<button class="btn" data-role="cancel">取消</button>{{/if}} </div> {{/if}} </div> </div>',
       width: 300
     },
 
--- dialog/src/dialog.tpl
+++ dialog/src/dialog.tpl
@@ -1,17 +1,17 @@
-<div class="window" style="left:50px;top:150px;">
+<div class="dialog" style="left:50px;top:150px;">
   {{#if hasCloseX}}<i class="icon icon-tool icon-tool-close" data-role="close"></i>{{/if}}
   {{#if hasTitle}}
-  <div class="window-header unselectable" data-role="head">
+  <div class="dialog-hd unselectable" data-role="head">
     <span data-role="title">{{title}}</span>
   </div>
   {{/if}}
-  <div class="window-body">
+  <div class="dialog-bd">
     {{#if icon}}
     <i class="icon icon-{{icon}}"></i>
     {{/if}}
     <span data-role="content">{{content}}</span>
     {{#if hasFoot}}
-    <div class="window-toolbar" data-role="foot">
+    <div class="dialog-toolbar" data-role="foot">
       {{#if hasOk}}<button class="btn" data-role="confirm">确定</button>{{/if}}
       {{#if hasCancel}}<button class="btn" data-role="cancel">取消</button>{{/if}}
     </div>
